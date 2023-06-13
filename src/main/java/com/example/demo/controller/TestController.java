979f3248b7d73ef8fb52a8ebaa8387aead4c46e9
Kim Na Young <pfskdud@sookmyung.ac.kr>
2023-06-13 10:34:58 +0900

    백엔드 개발 11. @RequestMapping, @GetMapping

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -16,6 +17,11 @@ public class TestController {
 	@GetMapping("/{id}")
 	public String testContollerWithPathVariables(@PathVariable(required =false) int id) {
 		return "Hello World! ID " + id;
+	}
+	
+	@GetMapping("/Param")
+	public String testContollerRequestParam(@RequestParam(required =false) int id) {
+		return "Hello World! ID param "+ id;
 	}
 
 }
